39c01554cefd4bef679fa435199ea9a78c67209b
unknown <alaeron@alaeron.com>
2019-12-07 18:15:36 -0800

    added feats

--- renderer.js
+++ renderer.js
@@ -8,6 +8,7 @@ var Templates = {}
 Templates.Spell = `
 <div class="item spell">
 <div class="title"></div>
+<div class="level"></div>
 <div class="school"></div>
 <i class="fas fa-angle-up"></i>
 <div class="info">
@@ -17,6 +18,17 @@ Templates.Spell = `
   <div class="duration"></div>
   <div class="description"></div>
 </div>
+<div class="edit-panel">                      
+  <i class="fas fa-edit"></i>
+  <i class="fas fa-minus-circle"></i>
+</div>
+</div>
+`
+Templates.Feat = `
+<div class="item feat">
+<div class="title"></div>
+<i class="fas fa-angle-up"></i>
+<div class="description"></div>
 <div class="edit-panel">                      
   <i class="fas fa-edit"></i>
   <i class="fas fa-minus-circle"></i>
@@ -69,6 +81,7 @@ function begin_edit_spell(itemElement) {
     itemElement.classList.add('editing')
     
     var titleElement = itemElement.querySelector('.title')
+    var levelElement = itemElement.querySelector('.level')
     var schoolElement = itemElement.querySelector('.school')
     var rangeElement = itemElement.querySelector('.range')
     var timeElement = itemElement.querySelector('.time')
@@ -77,6 +90,7 @@ function begin_edit_spell(itemElement) {
     var descriptionElement = itemElement.querySelector('.description')
 
     wrap_edit_property(titleElement, "Title")
+    wrap_edit_property(levelElement, "Level")
     wrap_edit_property(schoolElement, "School")
     wrap_edit_property(rangeElement, "Range")
     wrap_edit_property(timeElement, "Time")
@@ -91,27 +105,11 @@ function begin_edit_spell(itemElement) {
     saveElement.addEventListener("click", spell_save)
 
 }
-function wrap_edit_property(target, placeholder, tag, type) {
-    if (tag === undefined) {
-        tag = 'input'
-    }
-    var value = target.textContent
-    var editElement = document.createElement(tag)
-    editElement.className = "edit-property"
-    editElement.placeholder = placeholder
-    editElement.value = value
-    target.textContent = ''
-
-    if (type !== undefined) {
-        editElement.type = type
-    }
-
-    target.appendChild(editElement)
-}
 function spell_save(event) {
     var itemElement = event.target.closest('.item')
     
     var titleElement = itemElement.querySelector('.title')
+    var levelElement = itemElement.querySelector('.level')
     var schoolElement = itemElement.querySelector('.school')
     var rangeElement = itemElement.querySelector('.range')
     var timeElement = itemElement.querySelector('.time')
@@ -120,6 +118,7 @@ function spell_save(event) {
     var descriptionElement = itemElement.querySelector('.description')
 
     unwrap_edit_property(titleElement)
+    unwrap_edit_property(levelElement)
     unwrap_edit_property(schoolElement)
     unwrap_edit_property(rangeElement)
     unwrap_edit_property(timeElement)
@@ -129,10 +128,6 @@ function spell_save(event) {
     
     event.target.remove()
     itemElement.classList.remove('editing')
-}
-function unwrap_edit_property(target) {
-    var value = target.children[0].value
-    target.innerHTML = value
 }
 
 /* Item Delete */
@@ -144,22 +139,72 @@ function item_delete(event) {
     itemElement.remove()
 }
 
-/* Add Spell */
-document.querySelector(".sidebar .section.spells .item.add").addEventListener("click", add_spell)
+/* Add Feat */
+document.querySelector(".sidebar .section.feats .item.add").addEventListener("click", add_spell)
 function add_spell(event) {
-    var spellElement = createElementFromHTML(Templates.Spell)
-    var spellContainer = document.querySelector(".sidebar-right .section.spells .content .level")
+    var featElement = createElementFromHTML(Templates.Feat)
+    var featContainer = document.querySelector(".sidebar-right .section.feats .content")
 
-    spellElement.querySelector(".edit-panel .fa-edit").addEventListener("click", spell_edit)
-    spellElement.querySelector(".edit-panel .fa-minus-circle").addEventListener("click", item_delete)
-    spellElement.querySelector(".fa-angle-up").addEventListener("click", spell_toggle)
+    featElement.querySelector(".edit-panel .fa-edit").addEventListener("click", feat_edit)
+    featElement.querySelector(".edit-panel .fa-minus-circle").addEventListener("click", item_delete)
+    featElement.querySelector(".fa-angle-up").addEventListener("click", feat_toggle)
 
-    begin_edit_spell(spellElement)
+    begin_edit_feat(featElement)
 
-    spellContainer.insertBefore(spellElement, event.target)
+    featContainer.insertBefore(featElement, event.target)
+
+}
+function sortSpells() {
+    var spellContainer = document.querySelector(".sidebar-right .section.spells .content")
 
 }
 
+/* Feat Collapse */
+document.querySelectorAll(".sidebar .section .feat > i").forEach(function(element) {
+    element.addEventListener("click", spell_toggle)
+})
+function feat_toggle(event) {
+    event.target.parentElement.classList.toggle("collapsed")   
+    event.target.classList.toggle("fa-angle-up")
+    event.target.classList.toggle("fa-angle-down")
+}
+
+/* Feat Editing */
+document.querySelectorAll(".sidebar .section .feat .edit-panel > i.fa-edit").forEach(function(element) {
+    element.addEventListener("click", feat_edit)
+})
+function feat_edit(event) {
+    var itemElement = event.target.closest('.item')
+    begin_edit_feat(itemElement)
+}
+function begin_edit_feat(itemElement) {    
+    itemElement.classList.add('editing')
+    
+    var titleElement = itemElement.querySelector('.title')
+    var descriptionElement = itemElement.querySelector('.description')
+
+    wrap_edit_property(titleElement, "Title")
+    wrap_edit_property(descriptionElement, "Description", 'textarea')
+
+    var saveElement = document.createElement('i')
+    saveElement.classList = "fas fa-save"
+    itemElement.querySelector(".edit-panel").appendChild(saveElement)
+
+    saveElement.addEventListener("click", feat_save)
+
+}
+function feat_save(event) {
+    var itemElement = event.target.closest('.item')
+    
+    var titleElement = itemElement.querySelector('.title')
+    var descriptionElement = itemElement.querySelector('.description')
+
+    unwrap_edit_property(titleElement)
+    unwrap_edit_property(descriptionElement)
+    
+    event.target.remove()
+    itemElement.classList.remove('editing')
+}
 
 /* Utility Functions */
 function createElementFromHTML(htmlString) {
@@ -168,4 +213,27 @@ function createElementFromHTML(htmlString) {
   
     // Change this to div.childNodes to support multiple top-level nodes
     return div.firstChild; 
-  }
+}
+
+  function wrap_edit_property(target, placeholder, tag, type) {
+    if (tag === undefined) {
+        tag = 'input'
+    }
+    var value = target.innerHTML
+    var editElement = document.createElement(tag)
+    editElement.className = "edit-property"
+    editElement.placeholder = placeholder
+    editElement.value = value.replace(/<br>/g, "\n")
+    target.textContent = ''
+
+    if (type !== undefined) {
+        editElement.type = type
+    }
+
+    target.appendChild(editElement)
+}
+
+function unwrap_edit_property(target) {
+    var value = target.children[0].value.replace(/(?:\r\n|\r|\n)/g, '<br>')
+    target.innerHTML = value
+}
